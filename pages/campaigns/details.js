a11ff1a40fba835eafd362695849de2d6316482e
Ramon Zuniga <ramonzuniga9@gmail.com>
2022-01-06 19:22:55 -0600

    campaign details boilerplate page

---
+++ pages/campaigns/details.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class CampaignDetails extends Component {
+  render() {
+    return <h3>Campaign Details</h3>;
+  }
+}
+
+export default CampaignDetails;
