a2ba369c7b22133c4782608c2a1b820c7ded2522
Ramon Zuniga <ramonzuniga9@gmail.com>
2023-01-01 13:45:11 -0600

    Requests index page, create request form (template)

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ const routes = require("next-routes")();
 
 routes
   .add("/campaigns/new", "/campaigns/new")
-  .add("/campaigns/:address", "/campaigns/details");
+  .add("/campaigns/:address", "/campaigns/details")
+  .add("/campaigns/:address/requests", "/campaigns/requests/index")
+  .add("/campaigns/:address/requests/new", "/campaigns/requests/new");
 
 module.exports = routes;
